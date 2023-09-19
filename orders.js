7accc87d1a6873b71fd17910339e1816fef69eda
Caleb <126200524+smelly-cheese11@users.noreply.github.com>
2023-09-19 14:49:17 -0600

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Dark Themed Website',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Pineapple Themed Website',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'How to Make a Game in Unity',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
